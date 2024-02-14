4f234cc8a09f826704f76f9cefee1c5f85ee43f8
Evgeniy Goryunov <evgeniyrmcj@gmail.com>
2024-02-15 00:41:40 +0300

    Почистил шаблон компонента меню от мусора

--- local/templates/bx-template/components/bitrix/menu/menu-header/lang/ru/.description.php
+++ local/templates/bx-template/components/bitrix/menu/menu-header/lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS ['MENU_DOT_DEFAULT_NAME'] = "Вертикальное меню по умолчанию";
-$MESS ['MENU_DOT_DEFAULT_DESC'] = "Вертикальное меню по умолчанию";
+$MESS ['MENU_DOT_DEFAULT_NAME'] = "Меню в хедере";
+$MESS ['MENU_DOT_DEFAULT_DESC'] = "Меню в хедере";
 ?>
