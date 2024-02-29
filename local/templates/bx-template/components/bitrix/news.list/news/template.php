9ed5ac2318c97fe2ea474e67162944de653ca6d6
Evgeniy Goryunov <evgeniyrmcj@gmail.com>
2024-02-29 22:30:55 +0300

    Добавил отображение раздела в компоненте новости, работа с данными производится через result_modifier

--- local/templates/bx-template/components/bitrix/news.list/news/template.php
+++ local/templates/bx-template/components/bitrix/news.list/news/template.php
@@ -19,12 +19,11 @@ $this->setFrameMode(true);
         $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
         $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
         ?>
-
         <a href="#" id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="swiper-slide main-news__card">
             <div class="main-news__card-img">
                 <picture class="picture">
                     <source type="image/webp" srcset="<?=$arItem['PREVIEW_PICTURE']['SRC']?>"><img class="picture__img" src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>">
-                </picture><span class="main-news__card-plug">выставки и конференции</span>
+                </picture><span class="main-news__card-plug"><?= $arResult['SECT_INFO'][$arItem['IBLOCK_SECTION_ID']]['NAME'] ?></span>
             </div>
             <p class="main-news__card-date"><?= $arItem['DISPLAY_ACTIVE_FROM'];?></p>
             <p class="main-news__card-title"><?=$arItem['NAME'];?></p>
