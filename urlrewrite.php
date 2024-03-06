47836453279f3d85313498973b99b37b4ab530f4
Evgeniy Goryunov <evgeniyrmcj@gmail.com>
2024-03-07 02:44:54 +0300

    Новости с поддержкой ЧПУ готовы

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,5 +1,20 @@
 <?php
 $arUrlRewrite=array (
+  1 => 
+  array (
+    'CONDITION' => '#^/news/([\\w-]+)/([\\w-]+)/#',
+    'RULE' => 'ELEMENT_CODE=$2',
+    'PATH' => '/news/index.php',
+    'SORT' => 3,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/news/([\\w-]+)/#',
+    'RULE' => 'SECTION_CODE=$1',
+    'PATH' => '/news/index.php',
+    'SORT' => 3,
+  ),
+  0 => 
   array (
     'CONDITION' => '#^/rest/#',
     'RULE' => '',
@@ -7,16 +22,12 @@ $arUrlRewrite=array (
     'PATH' => '/bitrix/services/rest/index.php',
     'SORT' => 100,
   ),
-    array (
-        'CONDITION' => '#^/news/([\w-]+)/([\w-]+)/#',
-        'RULE' => "ELEMENT_CODE=$2",
-        'PATH' => '/news/index.php',
-        'SORT' => 3
-    ),
+  3 => 
   array (
-    'CONDITION' => '#^/news/([\w-]+)/#',
-    'RULE' => "SECTION_CODE=$1",
-    'PATH' => '/news/index.php',
-      'SORT' => 3
+    'CONDITION' => '#^#',
+    'RULE' => '',
+    'ID' => 'bitrix:news.list',
+    'PATH' => '/local/templates/bx-template/includes/pages/news/listing.php',
+    'SORT' => 100,
   ),
 );
