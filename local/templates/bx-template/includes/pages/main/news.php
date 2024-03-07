1ed63f20fa3a177e2583e3574e6e3f72035288b1
Evgeniy Goryunov <evgeniyrmcj@gmail.com>
2024-03-07 03:00:47 +0300

    Повесил ссылку на новости, на кнопку списка всех новостей на главной

--- local/templates/bx-template/includes/pages/main/news.php
+++ local/templates/bx-template/includes/pages/main/news.php
@@ -116,7 +116,7 @@
 			</div>
 		</div>
 		<div class="main-news__bot">
-            <a class="main-news__bot-button btn-hover_parent" href="#">
+            <a class="main-news__bot-button btn-hover_parent" href="/news">
 				<div class="btn-hover_circle"></div><span>Все новости и акции</span>
 			</a>
         </div>
